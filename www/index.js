345d006e93d59c2eb8e609cc09acc625062f9c1d
AaronCasildo <aacasildor@gmail.com>
2026-01-28 22:28:02 -0800

    feat: add example of exporting memory from wasm to js

--- www/index.js
+++ www/index.js
@@ -11,14 +11,20 @@ async function init(){
         }
 
     }
-            const response = await fetch("sum.wasm");
-            const buffer = await response.arrayBuffer();
-            // debugger
-            const wasm = await WebAssembly.instantiate(buffer,importObject);
+        const response = await fetch("sum.wasm");
+        const buffer = await response.arrayBuffer();
+        // debugger
+        const wasm = await WebAssembly.instantiate(buffer,importObject);
 
-            const sumFunction = wasm.instance.exports.sum;
-            const result = sumFunction(9, 9);
-            console.log("Result of sum(9, 9):", result);
-        }
+        const sumFunction = wasm.instance.exports.sum;
+        const wasmMemory = wasm.instance.exports.memory;
 
-        init();
+        const uint8Array = new Uint8Array(wasmMemory.buffer,0,40);
+        const hitext = new TextDecoder().decode(uint8Array);
+        console.log("Contents of WASM Memory:", hitext);
+
+        const result = sumFunction(9, 9);
+        console.log("Result of sum(9, 9):", result);
+    }
+
+    init();
--- www/public/sum.wat
+++ www/public/sum.wat
@@ -7,15 +7,20 @@
 ;;   (export "sum" (func $sum)))
 
 (module
-    (import "console" "log" (func $mylog))
-    (import "console" "error" (func $error))
-
-    (func $sum (param $a i32)(param $b i32) (result i32)
-        call $mylog
-        call $error
-        local.get $a
-        local.get $b
-        i32.add
-    )
+  (import "console" "log" (func $mylog))
+  (import "console" "error" (func $error))
+  (memory $mem 1) ;; this is the same as 1 page = 64KB
+  (data (i32.const 0) "Sup yall, this is WebAssembly!")
+  (func $sum (param $a i32)(param $b i32) (result i32)
+      call $mylog
+      call $error
+      local.get $a
+      local.get $b
+      i32.add
+  )
+  (export "memory" (memory $mem))
   (export "sum" (func $sum))
 )
+
+;;We can create memory in WebAssembly and export it to JS
+;;We can create memory in JS and import it to WebAssembly
