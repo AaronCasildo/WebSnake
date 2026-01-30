b6fa001da5f26944a74ffe608bb367bb224ff593
AaronCasildo <aacasildor@gmail.com>
2026-01-29 19:10:53 -0800

    chore: add initial function and compiling it from the lib.rs file

--- www/index.js
+++ www/index.js
@@ -1,34 +1,6 @@
-async function init(){
-    const memory = new WebAssembly.Memory({initial:1});
+import init, { greet } from "web_snake"
 
-    const importObject = {
-        js: {
-            mem:memory
-        },
-
-        console: {
-            log:() => {
-                console.log("Just logging stuff");
-            },
-            error: () => {
-                console.log("Error");
-            }
-        }
-
-    }
-        const response = await fetch("sum.wasm");
-        const buffer = await response.arrayBuffer();
-        debugger
-        const wasm = await WebAssembly.instantiate(buffer,importObject);
-        debugger
-        const sumFunction = wasm.instance.exports.sum;
-
-        const uint8Array = new Uint8Array(memory.buffer,0,40);
-        const hitext = new TextDecoder().decode(uint8Array);
-        console.log("Contents of WASM Memory:", hitext);
-
-        const result = sumFunction(9, 9);
-        console.log("Result of sum(9, 9):", result);
-    }
-
-    init();
+init().then(() => {
+    greet("Aaron Casildo");
+    console.log("WASM Loaded");
+});
